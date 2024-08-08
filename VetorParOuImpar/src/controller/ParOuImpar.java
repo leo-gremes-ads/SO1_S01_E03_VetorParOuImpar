1fe118248768dfdb14e5895d9dfbfd5a3782ece7
Léo Gremes <160618417+leo-gremes-ads@users.noreply.github.com>
2024-08-08 14:56:47 -0300

    Add files via upload

--- VetorParOuImpar/src/controller/ParOuImpar.java
+++ VetorParOuImpar/src/controller/ParOuImpar.java
@@ -9,15 +9,17 @@ public class ParOuImpar
 
     public void exibirParOuImpar(int[] vetor)
     {
+        System.out.println("----------------------------");
         for (int n: vetor)
         {
-            if (n % 2 == 1) {
-                System.out.println(n + " é impar");
+            if (n % 2 != 0) {
+                System.out.println(String.format("%11d", n) + " é impar");
             }
             else {
-                if (n % 10 == 0 && n != 0)
-                System.out.println(n + " é par e múltiplo de 10");
+                if (n % 10 == 0)
+                System.out.println(String.format("%11d", n) + " é par e múltiplo de 10");
             }
         }
+        System.out.println("----------------------------");
     }
 }
